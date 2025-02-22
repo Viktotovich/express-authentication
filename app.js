5c8e01668a023683120e84b2760a7a64a3f0688e
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-02-22 21:49:08 +0400

    Configured an external prod DB - different configuration to the last time. Additionally, added some scaffold for getting comfortable with auth

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+
+const db = require("./model/pool");
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("App running"));
