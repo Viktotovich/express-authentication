cad1570fdd5410fd7f7bfa666a350191ed61fd87
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-02-24 02:41:36 +0400

    Implemented the full user sign-up, log-in, sign-out cycle - using session and passport local strategy

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const path = require("node:path");
-const { Pool } = require("pg");
+const pool = require("./model/pool");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
 
 //ROUTES
 const signupRouter = require("./routes/signupRouter");
-
-const db = require("./model/pool");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -18,7 +17,67 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("pages/index"));
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.get("/", async (req, res) => {
+  res.render("pages/index", { user: req.user });
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
 app.use("/sign-up", signupRouter);
 
 app.listen(3000, () => console.log("App running"));
